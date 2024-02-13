744e43ce1f8600ecbdebac8af576c2600ed448bc
mearftameen <152913981+mearftameen@users.noreply.github.com>
2024-02-13 09:06:24 +0300

    Initial project commit

---
+++ book_club/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\ALQURSAN\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ALQURSAN\Desktop\books\bookclub1\book_club"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
